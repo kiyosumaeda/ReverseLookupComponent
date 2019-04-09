da184a389e4d2d7c7c3d04149bc0ad70789871f8
kiyosu.maeda <kiyosu.0407@gmail.com>
2019-04-09 20:19:01 +0900

    create editor folder

---
+++ Assets/Editor/ReverseLookup.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class ReverseLookup : EditorWindow {
+
+	private string filter;
+
+	[MenuItem("Tools/ReverseLookup")]
+	private static void Init() {
+		GetWindow<ReverseLookup>();
+	}
+
+	private void OnGUI() {
+		GUILayout.BeginHorizontal();
+		GUILayout.Label("Filter:", GUILayout.Width(45));
+		GUI.SetNextControlName( "filterField" );
+        filter = GUILayout.TextField( filter, "SearchTextField", GUILayout.Width( 120 ) );
+        GUI.FocusControl( "filterField" );
+        GUI.enabled = !string.IsNullOrEmpty( filter );
+        if ( GUILayout.Button( "Clear", "SearchCancelButton" ) )
+        {
+            filter = string.Empty;
+        }
+		GUILayout.Label(filter, GUILayout.Width(120));
+        GUI.enabled = true;
+        GUILayout.EndHorizontal();
+	}
+}
